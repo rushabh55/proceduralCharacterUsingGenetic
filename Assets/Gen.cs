826dcd82998e3c8fddb241eedb0cc6358ca16b7c
Rushabh Gosar <rushabh.techie@gmail.com>
2014-04-06 14:53:44 -0400

    no message

--- Assets/Gen.cs
+++ Assets/Gen.cs
@@ -1,5 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Xml.Serialization;
+using System.Collections.Generic;
+using GA;
+
+namespace GA
+{
+    class Chromosome
+    {
+        public int[] w = new int[6];
+        public float[] distance = new float[6];
+    }
+}
 
 public class Gen : MonoBehaviour {
     System.Random rand = new System.Random();
@@ -14,9 +26,17 @@ public class Gen : MonoBehaviour {
 
     int add_count, chromo_count = 0;
 
+    public GUIStyle _guiStyle;   
+
+    List<Chromosome> population = new List<Chromosome>();
+
+    public List<GameObject> _currentObjects = new List<GameObject>();
+
+    int breakForce = 100;
+
 	// Use this for initialization
 	void Start () {
-        var chromosome = new float[18, 18];
+        Chromosome chromosome = new Chromosome();
 		float x=(float)rand.NextDouble();
 		float y=(float)rand.NextDouble();
 		float z=(float)rand.NextDouble();
@@ -30,76 +50,98 @@ public class Gen : MonoBehaviour {
 		y=(float)rand.NextDouble();
 		z=(float)rand.NextDouble();
 		
-		chromosome[add_count,chromo_count]=x;
-		chromosome[add_count,++chromo_count]=y;
-		chromosome[add_count,++chromo_count]=z;
-		
-		
-		body = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		body.transform.position = new Vector3(16.11f, 0.7F, 5f);
-		body.transform.localScale = new Vector3(1,1,1);
+		body = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //body.transform.position = new Vector3(16.11f, 0.7F, 5f);
+        //body.transform.localScale = new Vector3(1,1,1);
 		body.gameObject.AddComponent("Rigidbody");
 		
 		x=(float)rand.NextDouble();
 		y=(float)rand.NextDouble();
 		z=(float)rand.NextDouble();
-		
-		chromosome[add_count,chromo_count]=x;
-		chromosome[add_count,++chromo_count]=y;
-		chromosome[add_count,++chromo_count]=z;
-		
-		arm = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		arm.transform.position = new Vector3(15.6f, 0.7F, 5f);
-		arm.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
+
+        arm = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //arm.transform.position = new Vector3(15.6f, 0.7F, 5f);
+        //arm.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
 		arm.gameObject.AddComponent("Rigidbody");
-		
-		arm1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		arm1.transform.position = new Vector3(16.6f, 0.7F, 5f);
-		arm1.transform.localScale = new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
+
+        arm1 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //arm1.transform.position = new Vector3(16.6f, 0.7F, 5f);
+        //arm1.transform.localScale = new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
 		arm1.gameObject.AddComponent("Rigidbody");
 		
 		x=(float)rand.NextDouble();
 		y=(float)rand.NextDouble();
 		z=(float)rand.NextDouble();
-		
-		chromosome[add_count,chromo_count]=x;
-		chromosome[add_count,++chromo_count]=y;
-		chromosome[add_count,++chromo_count]=z;
-		
-		leg = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		leg.transform.position = new Vector3(16.6f, 0.0F, 5f);
-		leg.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
+
+        leg = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //leg.transform.position = new Vector3(16.6f, 0.0F, 5f);
+        //leg.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
 		leg.gameObject.AddComponent("Rigidbody");
-		
-		
-		leg1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-		leg1.transform.position = new Vector3(15.6f, 0.0F, 5f);
-		leg1.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
+
+
+        leg1 = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+        //leg1.transform.position = new Vector3(15.6f, 0.0F, 5f);
+        //leg1.transform.localScale =  new Vector3(1 * (float)x,1 * (float) y,1 * (float)z);
 		leg1.gameObject.AddComponent("Rigidbody");	
 		
-		head.AddComponent<FixedJoint>().connectedBody = body.GetComponent<Rigidbody>();
-		body.AddComponent<FixedJoint>().connectedBody = leg.GetComponent<Rigidbody>();
-		body.AddComponent<FixedJoint>().connectedBody = leg1.GetComponent<Rigidbody>();
-		body.AddComponent<FixedJoint>().connectedBody = arm.GetComponent<Rigidbody>();
-		body.AddComponent<FixedJoint>().connectedBody = arm1.GetComponent<Rigidbody>();
+		head.AddComponent<HingeJoint>().connectedBody = body.GetComponent<Rigidbody>();
+        body.AddComponent<HingeJoint>().connectedBody = leg.GetComponent<Rigidbody>();
+        body.AddComponent<HingeJoint>().connectedBody = leg1.GetComponent<Rigidbody>();
+        body.AddComponent<HingeJoint>().connectedBody = arm.GetComponent<Rigidbody>();
+        body.AddComponent<HingeJoint>().connectedBody = arm1.GetComponent<Rigidbody>();
+
+   
 
 		head.rigidbody.freezeRotation = true;
-		body.rigidbody.freezeRotation = true;
-
-		body.AddComponent("CharacterMotor");
-		//head.rigidbody.useGravity = false;
-		//body.rigidbody.useGravity = false;
-		//arm.rigidbody.useGravity = false;
-		//arm1.rigidbody.useGravity = false;
-		//leg.rigidbody.useGravity = false;
-		//leg1.rigidbody.useGravity = false;
-
+		body.rigidbody.freezeRotation = true; 
 		body.gameObject.name = "body";
+
+        _currentObjects.Add(body);
+        _currentObjects.Add(head);
+        _currentObjects.Add(arm);
+        _currentObjects.Add(arm1);
+        _currentObjects.Add(leg);
+        _currentObjects.Add(leg1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (chromo_count);
 		camera.transform.LookAt(body.transform.position);
+        if (camera.GetComponent<SmoothFollow>().target == null)
+        {
+            camera.GetComponent<SmoothFollow>().target = body.transform;
+        }
+
+        foreach (var t in body.GetComponents<HingeJoint>())
+        {
+            t.breakTorque = breakForce;
+            t.breakForce = breakForce;
+        }
 	}
+
+    void OnGUI()
+    {
+        
+        if (GUI.Button(new Rect(Screen.width - 100, Screen.height - 50, 100, 50), "Reset", _guiStyle))
+        {
+            Reset();          
+        }
+        GUI.Label(new Rect(Screen.width - 100, 0, 100, 20), "The MAX Break Force");
+        breakForce = System.Convert.ToInt32(GUI.TextArea(new Rect(Screen.width - 100, 20, 100, 50), breakForce.ToString()));
+    }
+
+    void Reset()
+    {
+        foreach (var obj in _currentObjects)
+            GameObject.Destroy(obj);
+
+        _currentObjects = new List<GameObject>();
+        Start();
+    }
+
+    void OnJointBreak(float breakForce)
+    {
+        Debug.Log("Joint Break " + breakForce);
+        Reset();
+    }
 }
---
+++ Assets/Mapping.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mapping : MonoBehaviour {
+    public enum obstacleType
+    {
+        Cube = PrimitiveType.Cube,
+        Cylinder = PrimitiveType.Cylinder,
+        Capsule = PrimitiveType.Capsule,
+        Sphere = PrimitiveType.Sphere,
+        Quad = PrimitiveType.Quad
+    }
+    public Camera cam;
+    public obstacleType _obstacleType;
+    public Texture _tex;
+    Shader shader1;
+	// Use this for initialization
+	void Start () {
+        shader1 = Shader.Find("Diffuse");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.mousePresent )
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    RaycastHit rayHit;                     
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    if (Physics.Raycast(ray, out rayHit))
+                    {
+
+                        var obj = GameObject.CreatePrimitive((PrimitiveType) _obstacleType);
+                        obj.transform.position = rayHit.point;
+                        obj.transform.rotation = transform.rotation;
+                        obj.AddComponent("Rigidbody");
+                        obj.renderer.material.shader = shader1;
+                        obj.renderer.material.mainTexture = _tex;
+                    }
+                }
+            }
+        }
+
+        if (Input.touchCount > 0)
+        {
+            foreach (var t in Input.touches)
+            {
+                RaycastHit rayHit;
+                Ray ray = Camera.main.ScreenPointToRay(t.position);
+                if (Physics.Raycast(ray, out rayHit))
+                {
+
+                    var obj = GameObject.CreatePrimitive((PrimitiveType)_obstacleType);
+                    obj.transform.position = rayHit.point;
+                    obj.transform.rotation = transform.rotation;
+                    obj.AddComponent("Rigidbody");
+                    obj.renderer.material.shader = shader1;
+                    obj.renderer.material.mainTexture = _tex;
+                }
+            }
+        }
+	}
+
+    void OnGUI()
+    {
+        var objs = GameObject.FindObjectsOfType(typeof(GameObject));
+        GUI.Label(new Rect(0, 0, 225, 50), "Total Objects: " + objs.Length);
+    }
+
+}
